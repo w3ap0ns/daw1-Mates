3573f181cbca3a53144ce6cd2ca8b9418f2cba84
Rafael Armas <rarmas252@live.com>
2023-09-24 00:17:27 +0200

    Daw1 - Mod1 - Mates

---
+++ src/app.java
@@ -0,0 +1,31 @@
+public class app {
+
+    public static void main(String[] arg){
+
+        /*Escribe un programa en Java (Mates) que calcule y muestre el valor de la
+        suma, resta, producto, división y resto, de dos números enteros.*/
+
+        // Declaración de variables
+        int x=6,y=3;
+
+        // ********** Operaciónes *********
+
+        //Suma
+        int suma = x + y;
+        System.out.printf("La suma de %s y %s es igual a: %s %n",x,y,suma);
+
+        //Resta
+        int resta = x - y;
+        System.out.printf("La resta de %s y %s es igual a: %s %n",x,y,resta);
+
+        //Multiplicación
+        int multiplicacion = x * y;
+        System.out.printf("La multiplicación de %s y %s es igual a: %s %n",x,y,multiplicacion);
+
+        //División
+        int division = x / y;
+        System.out.printf("La división de %s y %s es igual a: %s %n",x,y,division);
+
+    } // public static void main(String[] arg)
+
+} // public class app
